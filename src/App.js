9edf119aae5cabcd7ac63038f365c12381131b42
Cation94 <52714837+Cation94@users.noreply.github.com>
2020-05-04 07:45:44 +0900

    add button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import TodoListTemplate from './components/TodoListTemplate.jsx'
+import Form from './components/Form'
 function App() {
   return (
     <div>
-        <TodoListTemplate>
+        <TodoListTemplate form={<Form/>}>
           테믈릿 완성!
         </TodoListTemplate>
     </div>
